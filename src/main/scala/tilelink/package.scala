2478f929f5dddef06f3122bf3a94651705f25aaf
Henry Cook <henry@sifive.com>
2020-09-18 19:00:23 -0700

    differentiate ClockCrossingHelpers from ResetCrossingHelpers

--- src/main/scala/tilelink/package.scala
+++ src/main/scala/tilelink/package.scala
@@ -2,8 +2,8 @@
 
 package freechips.rocketchip
 
-import Chisel._
-import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.diplomacy.{HasClockDomainCrossing, _}
+import freechips.rocketchip.prci.{HasResetDomainCrossing}
 
 package object tilelink
 {
@@ -18,8 +18,15 @@ package object tilelink
   type TLClientPortParameters = TLMasterPortParameters
 
   implicit class TLClockDomainCrossing(val x: HasClockDomainCrossing) extends AnyVal {
-    def crossIn (n: TLInwardNode) (implicit valName: ValName) = TLInwardCrossingHelper (valName.name, x, n)
-    def crossOut(n: TLOutwardNode)(implicit valName: ValName) = TLOutwardCrossingHelper(valName.name, x, n)
+    def crossIn (n: TLInwardNode) (implicit valName: ValName) = TLInwardClockCrossingHelper (valName.name, x, n)
+    def crossOut(n: TLOutwardNode)(implicit valName: ValName) = TLOutwardClockCrossingHelper(valName.name, x, n)
+    def cross(n: TLInwardNode) (implicit valName: ValName) = crossIn(n)
+    def cross(n: TLOutwardNode)(implicit valName: ValName) = crossOut(n)
+  }
+
+  implicit class TLResetDomainCrossing(val x: HasResetDomainCrossing) extends AnyVal {
+    def crossIn (n: TLInwardNode) (implicit valName: ValName) = TLInwardResetCrossingHelper (valName.name, x, n)
+    def crossOut(n: TLOutwardNode)(implicit valName: ValName) = TLOutwardResetCrossingHelper(valName.name, x, n)
     def cross(n: TLInwardNode) (implicit valName: ValName) = crossIn(n)
     def cross(n: TLOutwardNode)(implicit valName: ValName) = crossOut(n)
   }
